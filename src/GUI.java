e1f6a49fbd5648451c4ab5c5eb007573a97a117f
megera11 <marekhajduczenia11@gmail.com>
2021-03-03 13:59:13 +0100

    Pierwszy

---
+++ src/GUI.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import javax.swing.GroupLayout;
+/*
+ * Created by JFormDesigner on Wed Mar 03 13:30:22 CET 2021
+ */
+
+
+
+/**
+ * @author unknown
+ */
+public class GUI extends JPanel {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+
+    public GUI() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+
+        //======== this ========
+        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax
+        . swing. border. EmptyBorder( 0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax. swing
+        . border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java .awt .
+        Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ), java. awt. Color. red
+        ) , getBorder( )) );  addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override
+        public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062ord\u0065r" .equals (e .getPropertyName (
+        ) )) throw new RuntimeException( ); }} );
+
+        GroupLayout layout = new GroupLayout(this);
+        setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup()
+                .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup()
+                .addGap(0, 300, Short.MAX_VALUE)
+        );
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - unknown
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculator");
+        frame.setContentPane(new GUI().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
